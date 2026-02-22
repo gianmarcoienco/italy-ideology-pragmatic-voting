add42f52e5346d0722cbf12d8139b32e09611695
gianmarcoienco <gianmarcoienco23@gmail.com>
2026-02-22 16:56:13 +0100

    Update 00_run_project.R

--- 00_run_project.R
+++ 00_run_project.R
@@ -1,55 +1,21 @@
 # ==========================================
 # 00_run_project.R
-# Entry point for the project pipeline
+# Main entry point
 # ==========================================
 
-#clear console
+rm(list = ls())
 cat("\014")
 
-#clear all globals in memory
-rm(list = ls()) 
+source("00_config.R")
 
-library(rstudioapi)
-MAINNAME <- "run_project"
-if (requireNamespace("rstudioapi", quietly = TRUE)) {
-  ctx <- try(rstudioapi::getActiveDocumentContext(), silent = TRUE)
-  if (!inherits(ctx, "try-error") && nchar(ctx$path) > 0) {
-    MAINNAME <- basename(ctx$path)
-    MAINNAME <- sub("\\.R$", "", MAINNAME)
-  }
-}
+# Run the pipeline
+source(file.path(DO, "parties.R"), echo = TRUE, max = 1000)
+source(file.path(DO, "historical.R"), echo = TRUE, max = 1000)
+source(file.path(DO, "national.R"), echo = TRUE, max = 1000)
+source(file.path(DO, "administrative.R"), echo = TRUE, max = 1000)
+source(file.path(DO, "controls_first.R"), echo = TRUE, max = 1000)
+source(file.path(DO, "controls_second.R"), echo = TRUE, max = 1000)
+source(file.path(DO, "analysis.R"), echo = TRUE, max = 1000)
 
-# Project root assumed to be the working directory
-HOME <- normalizePath(getwd(), winslash = "/")
-DO <- paste0(HOME, "/c_program/")
-
-
-######################+
-# launch set-up scripts #####
-input <- '00_intro.R'
-source(paste0(DO,input,sep=""), echo=TRUE, max=1000)
-#DEBUG <- T
-
-
-################################ SCRIPT #########################################
-
-input <- 'parties.R'
-source(paste0(DO,input,sep=""), echo=TRUE, max=1000)
-
-input <- 'historical.R'
-source(paste0(DO,input,sep=""), echo=TRUE, max=1000)
-
-input <- 'national.R'
-source(paste0(DO,input,sep=""), echo=TRUE, max=1000)
-
-input <- 'administrative.R'
-source(paste0(DO,input,sep=""), echo=TRUE, max=1000)
-
-input <- 'controls_first.R'
-source(paste0(DO,input,sep=""), echo=TRUE, max=1000)
-
-input <- 'controls_second.R'
-source(paste0(DO,input,sep=""), echo=TRUE, max=1000)
-
-input <- 'analysis.R'
-source(paste0(DO,input,sep=""), echo=TRUE, max=1000)
+# Optional extras (do NOT run by default)
+# source(file.path(HOME, "bayesian_stan.R", "bayesian_stan2.R"), echo = TRUE, max = 1000)o=TRUE, max=1000)
